8d4daf799c310222f76e8af5b9b0da115078eb69
lislie <khalilfong@Live.cn>
2019-03-08 17:26:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3006;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Ether\n');
+    res.end('Hello change\n');
 });
 
 server.listen(port, hostname, () => {
