1bee133b1725c1383d14eee1e1d459e97575338a
lislie <khalilfong@Live.cn>
2019-03-11 17:48:57 +0800

    11111

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3006;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello are you ok? \n');
+    res.end('Hello are\n');
 });
 
 server.listen(port, hostname, () => {
